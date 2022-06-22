4e1b08492d87fa7e3b032a5cd327e8f1652101b6
Faez-B <faezbhatti@gmail.com>
2022-06-22 09:26:59 +0200

    Update index.js : Suppression de commentaires

--- index.js
+++ index.js
@@ -18,30 +18,8 @@ app.use(express.json());
 app.use(cors({
     // exposedHeaders: "x-auth-token",
     origin: "*"
-    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    // preflightContinue: true,
-    // optionsSuccessStatus: 204
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
 }))
-
-// Add headers before the routes are defined
-// app.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//     // Request headers you wish to allow
-//     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     // res.setHeader('Access-Control-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
 
 let _id = 1;
 
